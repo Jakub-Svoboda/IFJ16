3bb4134031a27f4b1535168d72060d9dc1946c21
Michael-Schmid <Mysic3k@seznam.cz>
2016-10-14 22:13:21 +0200

    argc warning solved

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 #include "syntax.h"
 
 int main(int argc, char *argv[]){
-	FILE *f;
-	f = fopen(argv[1], "r");
-	runPrecedenceAnalysis(f);
-	fclose(f);
+	if(argc==2)
+	{
+		FILE *f;
+		f = fopen(argv[1], "r");
+		runPrecedenceAnalysis(f);
+		fclose(f);
+	}
 	return 0;
 }
