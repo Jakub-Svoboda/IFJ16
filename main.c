771d0cb2684e378f5a85b4ff6c63b6437944a416
Michael-Schmid <Mysic3k@seznam.cz>
2016-10-14 21:29:47 +0200

    Pridani zpracovani argumentu do mainu

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include "syntax.h"
 
-int main(){
-	printf("Hello world!!!\n");
-	runPrecedenceAnalysis();
-	
+int main(int argc, char *argv[]){
+	FILE *f;
+	f = fopen(argv[1], "r");
+	runPrecedenceAnalysis(f);
+	fclose(f);
 	return 0;
 }
