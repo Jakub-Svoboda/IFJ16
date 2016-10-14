344923ed68c0a02e02c47cd84ad8d21c9ccc88dd
Jakub-Svoboda <Svo.jakub@seznam.cz>
2016-10-14 20:18:58 +0200

    precedence table added

--- syntax.c
+++ syntax.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "syntax.h"
+
+
 char precedence_table[14][14]={					//TODO Why is the range 14 though? 
 //input 	
 //+   -   *   /   <   >  <=  >=  ==  !=	  (	  )   i   $ 	  
@@ -19,30 +21,39 @@ char precedence_table[14][14]={					//TODO Why is the range 14 though?
 {'>','>','>','>','>','>','>','>','>','>','e','>','e','>'},	//i
 {'<','<','<','<','<','<','<','<','<','<','<','e','<','e'},	//$       	stack
 };
+void reduction(char *c){
+	char reductionn;
+	switch (c){
+		case "<E+E>":
+			reduction = "E"	;
+			break;
+	}
+}
 
-void stackPush(tStack* s,char inputChar){
+
+void stackPush(tStack *s,char inputChar){
 	s->top=s->top+1;
 	s->arr[s->top]=inputChar;	
 }
 
-void stackPop(tStack* s){
+void stackPop(tStack *s){
 	if(stackEmpty(s)){													//check for empty stack
-		printf("StackPop requested but stack is empty.\n");
+		fprintf(stderr,"StackPop requested but stack is empty.\n");
 	}else{
 		s->arr[s->top] ='0';
 		(s->top)--;
 	}	
 }
 
-void stackTop(tStack* s, char* c){
+void stackTop(tStack *s, char *c){
 	if(stackEmpty(s)){													//check for empty stack
-		printf("StackTop requested but stack is empty.\n");
+		fprintf(stderr,"StackTop requested but stack is empty.\n");
 	}else{
 		*c=s->arr[s->top];
 	}	
 }
 
-int stackEmpty (tStack* s){
+int stackEmpty (tStack *s){
 	return(MAX_STACK-1 == s->top);
 }
 
--- syntax.h
+++ syntax.h
@@ -1,8 +1,13 @@
 #define MAX_STACK 1023
 
 typedef struct{
-	char arr[MAX_STACK][1];		//1024 2 when indexed from 0
+	char arr[MAX_STACK];		//1024 2 when indexed from 0
 	int top;
 }tStack;
 
-int runSyntax();
+void stackPush(tStack* s,char inputChar);
+void stackPop(tStack* s);
+void stackTop(tStack* s, char* c);
+int stackEmpty(tStack* s);
+int runPrecedenceAnalysis();
+void reduction(char c[]);
