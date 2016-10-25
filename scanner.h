12737cc7625185a2b4c9522aba461402823d6c99
Michael-Schmid <Mysic3k@seznam.cz>
2016-10-25 18:29:38 +0200

    dopsano cislovani

--- scanner.h
+++ scanner.h
@@ -2,64 +2,63 @@
 
 typedef enum{
 	//ArOp
-	token_add,
-	token_subtract,
-    token_multiply,
-    token_divide,	
-
+	token_add,			//0
+	token_subtract,			//1
+    token_multiply,			//2
+    token_divide,			//3
 	//RelOp
-	token_less,
-    token_greater,
-    token_equal,
-	token_lessEqual,
-    token_greaterEqual,
-    token_notEqual,
-	token_bracketLeftRound,
-    token_bracketRightRound,
-	token_identifier,
-	token_dollar,				//Kuba added this
-	token_expression,			//Kubba added this, not sure if needed?
+	token_less,			//4
+    token_greater,			//5
+    token_equal,			//6
+	token_lessEqual,		//7
+    token_greaterEqual,			//8
+    token_notEqual,			//9
+	token_bracketLeftRound,		//10
+    token_bracketRightRound,		//11
+	token_identifier,		//12
+	token_dollar,			//13	Kuba added this
+	token_expression,		//14	Kubba added this, not sure if needed?
 
 	
-    token_invalid,
-    token_assign,
-    token_EOF,
+    token_invalid,			//15
+    token_assign,			//16
+    token_EOF,				//17
     //Keywords
-    token_boolean,
-    token_break,
-    token_class,
-    token_continue,
-    token_do,
-    token_double,
-    token_else,
-    token_false,
-    token_for,
-    token_if,
-    token_int,
-    token_return,
-    token_String,
-    token_static,
-    token_true,
-    token_void,
-    token_while,
+    token_boolean,			//18
+    token_break,			//19
+    token_class,			//20
+    token_continue,			//21
+    token_do,				//22
+    token_double,			//23
+    token_else,				//24
+    token_false,			//25
+    token_for,				//26
+    token_if,				//27
+    token_int,				//28
+    token_return,			//29
+    token_String,			//30
+    token_static,			//31
+    token_true,				//32
+    token_void,				//33
+    token_while,			//34
 
     //Delimiters
-    token_dot,
-    token_comma,
-    token_bracketLeftCurly,
-    token_bracketRightCurly,
-    token_semicolon,
-    token_bracketLeftSquare,
-    token_bracketRightSquare,
+    token_dot,				//35
+    token_comma,			//36
+    token_bracketLeftCurly,		//37
+    token_bracketRightCurly,		//38
+    token_semicolon,			//39
+    token_bracketLeftSquare,		//40
+    token_bracketRightSquare,		//41
     //"" ''
-    token_quotesSingle,
-    token_quotesDouble,
-    token_string,
-    token_intNumber,
-    token_doubleNumber,
+    token_quotesSingle,			//42
+    token_quotesDouble,			//43
+    token_string,			//44
+    token_intNumber,			//45
+    token_doubleNumber,			//46
 	
-	token_leftHandle,	//Kuba added this
-	token_rightHandle	//Kuba added this
+	token_leftHandle,		//47	Kuba added this
+	token_rightHandle		//48	Kuba added this
 } Token_type;
 
 
