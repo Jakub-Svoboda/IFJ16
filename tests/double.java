3eac474923e25ae03b058e5df9fb7f8bdfef1393
Grssmnn <hogs1397@gmail.com>
2016-10-27 22:16:24 +0200

    Moved tests to folder, Q: delete old files?

---
+++ tests/double.java
@@ -0,0 +1,4 @@
+double kekel = 14.54;
+double kek = 14.4e+3;
+double keke = 10e-10;
+double kekelek = 1e4242;
---
+++ tests/lex.java
@@ -0,0 +1,5 @@
++ - * /
+< > == <= >= != ( ) sumId $ //expression?
+=
+boolean break class continue do double else false for if int return String static true void while
+. , { } ; [ ] "" '' 42 42e-42
