a555bb159cc7f45d499f0c3243b8ceda5e700dd2
Grssmnn <hogs1397@gmail.com>
2016-10-29 12:50:22 +0200

    Commented everything

--- scanner.c
+++ scanner.c
@@ -12,20 +12,20 @@
 char *keywords[] = {"boolean", "break", "class", "continue", "do", "double",
 	"else", "false", "for", "if", "int", "return", "String", "static", "true","void","while"};
 
-int isKeyword(char *string) {
+int isKeyword(char *string) {							//return index value of choosen keyword in array
     int i;
     for (i = 0; i < 17; i++) {
         if (!strcmp(keywords[i], string))
             return i;
     }
-    return -1;
+    return -1;											//if string is not keyword return -1
 }
 
-Token *tokenInit() {
+Token *tokenInit() {									//allocate space
 	Token *t = (Token *)malloc(sizeof(Token));
 	if(t == NULL) {
-		printf("tokenInit malloc error\n");
-		t->type = token_invalid;
+		printf("tokenInit malloc error\n");				//propably BS
+		t->type = token_invalid;						//not sure if this is ok since there is only NULL in t, so it can't have ->type
 	}
 	return t;
 }
@@ -60,17 +60,17 @@ Token *lookAhead(FILE *f, int steps) { //TODO : Is there better way of passing F
 	Token *t;
 	switch(steps) {
 		case 0:
-			if(tNext == NULL) {
+			if(tNext == NULL) {			//lookAhead(1) hasn't been called last time, getToken is needed   ;;IDK if tenses are OK, bad english
 				t = getToken(f);
-			}else {
+			}else {						//lookAhead(1) has been called, return token that is already read
 				t = tNext;
-				tNext = NULL;
+				tNext = NULL;			//NULL static token tNext because its value is currently returned
 			}
 			return t;
 			break;
 		case 1:
 			//t = getToken(f);
-			tNext = getToken(f);
+			tNext = getToken(f);		//to look onto next token call lookAhead(1), it can't skip token, it can only look to next
 			return tNext;
 			break;
 		default:
@@ -84,12 +84,12 @@ Token *getToken(FILE *f) { 	//TODO : Is there better way of passing FILE? 	//Cal
 	//printf("AYA");
 	Token *t = tokenInit();	//TODO Kuba-edit
 	char buff[1024];
-    int c, position = 0, tempc, kwIndex, isDouble;
-	State_type state = state_default;
+    int c, position = 0, tempc, kwIndex, isDouble;		//tempc is buffered character, kwIndex is ord. value of keyword type, isDouble is boolean-like var
+	State_type state = state_default;					//choose between states
 	//t = malloc(sizeof(Token));
 	//tokenInit(t);
 	//t->type = token_invalid;
-	if(t->type == token_invalid){
+	if(t->type == token_invalid){						//initialization faiulure
 		printf("Invalid token\n");
 		return t;
 	}
@@ -98,73 +98,76 @@ Token *getToken(FILE *f) { 	//TODO : Is there better way of passing FILE? 	//Cal
 	while(1) {					//TODO ?
 		c = fgetc(f);
 		//printf("%c,",c);
-		switch (state) {
+		switch (state) {								//check if I'm not in reading number/id/string phase
 			case state_readingIdentifier:
-				if(isalpha(c) || isdigit(c)){
+				if(isalpha(c) || isdigit(c)){			//id's may contain numbers and characters
 					buff[position] = c;
 					position++;
-				}else {
-					state = state_default;
-					ungetc(c,f);
+				}else {									//end of allowed chars
+					state = state_default;				//set state to default so we'll know we are not reading id anymore
+					ungetc(c,f);						//back 1 char
 				}
-				if(state == state_default) {
-					buff[position] = '\0';
-					if((kwIndex = isKeyword(buff)) != -1) {
-						t->type = kwIndex + KEYWORD_OFFSET;
+				if(state == state_default) {			//reading has ended
+					buff[position] = '\0';				//add ending character at the end
+					if((kwIndex = isKeyword(buff)) != -1) {		//if buffered word is in keyword array return kw token
+						t->type = kwIndex + KEYWORD_OFFSET;		//value calculated by returned kwIndex and KEYWORD_OFFSET set in scanner.h
 					}else {
-						t->type = token_identifier;
+						//TODO add buffered string to hashtable and link pointer
+						t->type = token_identifier;				//it's not keyword so return as id
 					}
-					position = 0;
+					position = 0;								//reset position of buffer
 					return t;
 				}
 				break;
 			case state_readingString:
 				buff[position] = c;
 				position++;
-				if(c == '\"' && buff[position-2] != '\\') {
-					state = state_default;
+				if(c == '\"' && buff[position-2] != '\\') {		//looking for " but only if previous char is not '\', so '\"' is not matching
+					state = state_default;						//if found, end reading
 				}
-				if(state == state_default) {
+				if(state == state_default) {			//same as readingIdentifier
 					buff[position] = '\0';
 					t->type = token_string;
-						printf("'%s'",buff);
+					//TODO add string into hashtable and link it to token
+					printf("'%s'",buff);				//remove when 100% working
 					position = 0;
 					return t;
 				}
 				break;
 			case state_readingNumber:
-				if(c == '.' || c == 'e' || c == 'E') {
+				if(c == '.' || c == 'e' || c == 'E') {	//when any character is <- , it is not average number, it is DOUBLE
 					isDouble = true;
 				}
 
-				if(isdigit(c)){
+				if(isdigit(c)){							//not sure why I did if and else if,.. when it does the same thing
 					buff[position] = c;
 					position++;
 				}else if(isDouble && (c == 'E' || c == 'e' || c == '+' || c == '-' || c == '.')){
 					buff[position] = c;
 					position++;
-				}else { //Osetrit double
+				}else { //Osetrit double				//is this OK now?
 					ungetc(c,f);
-					state = state_default;
-					//readingNumber = false;
+					state = state_default;				//end reading state
 				}
 				if(state == state_default) {
 					buff[position] = '\0';
 					if(isDouble) {
 						t->type = token_doubleNumber;
-						printf("DABL'%s'",buff);
+						//TODO hashtable insert
+						printf("DABL'%s'",buff);		//shout out its double
 					}else {
 						t->type = token_intNumber;
-						printf("INT'%s'",buff);
+						//TODO hashtable insert
+						printf("INT'%s'",buff);			//its just number..
 					}
-					position = 0;
-					isDouble = false;
+					position = 0;						//reset
+					isDouble = false;					//reset
 					return t;
 				}
 				break;
-			case state_default:
+			case state_default:									//this will be done at every beginning of token reading
 				switch (c) {
-					case ' ':
+					case ' ':									//skip trash like spaces, newlines and tabs
 						break;
 					case '\n':
 						break;
@@ -183,29 +186,29 @@ Token *getToken(FILE *f) { 	//TODO : Is there better way of passing FILE? 	//Cal
 					//Division or Comments
 					case '/':
 						c = fgetc(f);
-						if(c == '/') {
-							while((c=fgetc(f)) != '\n'){
+						if(c == '/') {							//line-comment found
+							while((c=fgetc(f)) != '\n'){		//just skip it!
 								;
 							}
 							break;
-						}else if(c == '*') {
+						}else if(c == '*') {					//block-comment found
 							//printf("K");
 							tempc = ' ';
-							while(((c=fgetc(f)) != '/') || (tempc != '*')) {
+							while(((c=fgetc(f)) != '/') || (tempc != '*')) {	//read until the combination of */ is found
 								tempc = c;
 							}
 							break;
-						}else {
-							ungetc(c,f);
+						}else {									//next char is not matching any comment combination
+							ungetc(c,f);						//move 1 char back and return token type
 							t->type = token_divide;
 							return t;
 						}
-					case '\'':
+					case '\'':									//TODO how to handle chars? does java require only "" for strings?
 						break;
-					case '\"':
+					case '\"':									//beggining of " had been found, set state to readingString and buffer the char
 						state = state_readingString;
 						buff[position] = c;
-						position++;
+						position++;								//increment position of buffer
 						break;
 					//Delimiters
 					case '.':
@@ -242,12 +245,12 @@ Token *getToken(FILE *f) { 	//TODO : Is there better way of passing FILE? 	//Cal
 							t->type = token_equal;
 							return t;
 						}
-						ungetc(c,f);
+						ungetc(c,f);								//same thing again and again , if it's not matching, move 1 char back and return token
 						t->type = token_assign;
 						return t;
 					//notEqual
 					case '!':
-						tempc = c;
+						tempc = c;			//not sure what does this mean, maybe I'll delete it later
 						c=fgetc(f);
 						if(c == '=') {
 							t->type = token_notEqual;
@@ -274,16 +277,16 @@ Token *getToken(FILE *f) { 	//TODO : Is there better way of passing FILE? 	//Cal
 						ungetc(c,f);
 						t->type = token_greater;
 						return t;
-					case EOF:
+					case EOF:										//Damn we made it to EOF; ggwp
 						t->type = token_EOF;
 						return t;
 					default:
-						if(isdigit(c)){
+						if(isdigit(c)){								//if c is number, set state to readingNumber
 							state = state_readingNumber;
-						}else {
+						}else {										//strings and numbers are alredy handled, it must be ID or KW
 							state = state_readingIdentifier;
 						}
-						buff[position] = c;
+						buff[position] = c;							//buffer it
 						position++;
 						break;
 				}
