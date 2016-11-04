ec81bb326a43fa75e353896d6712a7605a00137f
Matyas Sladek <sladek.matyas@seznam.cz>
2016-11-04 20:14:02 +0100

    Adding unfinished qsort and bma.

---
+++ bma.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int min(int a, int b);
+int max(int a, int b);
+void compute_jumps(char *P, int *CharJump);
+void compute_match_jump(char *P, int *MatchJump);
+int bma(char *P, char *T, int *CharJump, int *MatchJump);
+
