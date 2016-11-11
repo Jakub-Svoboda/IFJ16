101fbb6af286e71c228da5a228f357b75652b1ea
Michael-Schmid <Mysic3k@seznam.cz>
2016-11-11 15:56:41 +0100

    test script corrected

--- exittest.sh
+++ exittest.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 make
+
 for i in `ls tests/1/`; do
 ./project tests/1/$i 2> /dev/null
 if [ "$?" -ne "1" ]
 then 
 echo "tests/1" $i
 echo Neco je spatne
+echo Navratova hodnota: $?
 fi
 done
 
@@ -15,6 +17,7 @@ if [ "$?" -ne "2" ]
 then 
 echo "tests/2" $i
 echo Neco je spatne
+echo Navratova hodnota: $?
 fi
 done
 
@@ -24,5 +27,16 @@ if [ "$?" -ne "3" ]
 then 
 echo "tests/3" $i
 echo Neco je spatne
+echo Navratova hodnota: $?
+fi
+done
+
+for i in `ls tests/correct/`; do
+./project tests/correct/$i 2> /dev/null
+if [ "$?" -ne "0" ]
+then 
+echo "tests/correct" $i
+echo Neco je spatne
+echo Navratova hodnota: $?
 fi
 done
