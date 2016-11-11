f5c04841986b0a2202c1271d1ea43a5aae6d3765
Jakub-Svoboda <Svo.jakub@seznam.cz>
2016-11-11 17:19:29 +0100

    included the interpret this time

---
+++ interpret.c
@@ -0,0 +1,7 @@
+#include "interpret.h"
+
+int runInterpret(tListOfInstr *list){
+	fprintf(stderr,"Im doing all the stuff interprets do.\n");
+
+return 0;	
+}
---
+++ interpret.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "iList.h"
+
+int runInterpret(tListOfInstr *list);
