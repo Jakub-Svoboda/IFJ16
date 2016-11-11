834e98afe694f34359039b3f446f7c2a756bb3a4
Michael-Schmid <Mysic3k@seznam.cz>
2016-11-12 00:02:56 +0100

    test script bugfixes

--- exittest.sh
+++ exittest.sh
@@ -3,40 +3,44 @@ make
 
 for i in `ls tests/1/`; do
 ./project tests/1/$i 2> /dev/null
-if [ "$?" -ne "1" ]
+retval=$?
+if [ "$retval" -ne "1" ]
 then 
 echo "tests/1" $i
 echo Neco je spatne
-echo Navratova hodnota: $?
+echo Navratova hodnota: $retval
 fi
 done
 
 for i in `ls tests/2/`; do
 ./project tests/2/$i 2> /dev/null
-if [ "$?" -ne "2" ]
+retval=$?
+if [ "$retval" -ne "2" ]
 then 
 echo "tests/2" $i
 echo Neco je spatne
-echo Navratova hodnota: $?
+echo Navratova hodnota: $retval
 fi
 done
 
 for i in `ls tests/3/`; do
 ./project tests/3/$i 2> /dev/null
-if [ "$?" -ne "3" ]
+retval=$?
+if [ "$retval" -ne "3" ]
 then 
 echo "tests/3" $i
 echo Neco je spatne
-echo Navratova hodnota: $?
+echo Navratova hodnota: $retval
 fi
 done
 
 for i in `ls tests/correct/`; do
 ./project tests/correct/$i 2> /dev/null
-if [ "$?" -ne "0" ]
+retval=$?
+if [ "$retval" -ne "0" ]
 then 
 echo "tests/correct" $i
 echo Neco je spatne
-echo Navratova hodnota: $?
+echo Navratova hodnota: $retval
 fi
 done
