05337da3f6a55fe1fb509fd571673f37c82534fb
Grssmnn <hogs1397@gmail.com>
2016-11-19 14:26:12 +0100

    Few builtin tests

---
+++ tests/builtin/10.java
@@ -0,0 +1,20 @@
+class Main {
+	static void run() {
+		String a; String b; String klop;
+		a = "středníček";
+		b = "klopetek";
+		klop = "klop";
+
+		int num0; int num1; int num2; int num3; int num4;
+
+		num0 = ifj16.find(b, a);
+		num1 = ifj16.find(b, klop);
+		num2 = ifj16.find(a, a);
+		num3 = ifj16.find("klopetek", b);
+		num4 = ifj16.find("klopetek", klop);
+
+		ifj16.print(" a je " + a + " b je " + b + "\n");
+		ifj16.print("(b,a) " + num0 + "(b, klop) " + num1 + "(a,a) " + num2 + "(\"klopetek\",b) " + num3 +"(\"klopetek\",klop) " + num4)
+
+	}
+}
