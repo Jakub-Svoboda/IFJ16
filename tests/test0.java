8d55cd168bfc236ed4d61f7df82cbff79288e54b
Jakub-Svoboda <Svo.jakub@seznam.cz>
2016-11-29 19:35:38 +0100

    test update

--- tests/test0.java
+++ tests/test0.java
@@ -2,14 +2,16 @@
 
 class Main{
 	static void run(){
-		int a^a;			// exit 2, token je ale invalid	> exit 1
-		int #a;				// Toto projde jako id....
-		int a&a;			//exit 2, token je ale invalid	> exit 1
-		int a\a;			//exit 2, token je ale invalid	> exit 1
+	//	int a^a;					//fixed			// exit 2, token je ale invalid	> exit 1
+	//	int #a;						//fixed			// Toto projde jako id....
+	//	int a&a;					//fixed			//exit 2, token je ale invalid	> exit 1
+	//	int a\a;					//fixed			//exit 2, token je ale invalid	> exit 1
 		int 8a;				//dojde 45 intNumber, ale cislo je invalid > exit 1
-		int ř;				//exit 2, token je ale invalid	> exit 1
-		int h4ck0;				//projde
-		int h4ck0řiNámR0zbiliL3x;	//syntax
+	//	int ř;						//fixed			//exit 2, token je ale invalid	> exit 1
+	//	int h4ck0;					//fixed			//projde
+	//	int h4ck0řiNámR0zbiliL3x;	//fixed			//syntax
+	//double d = 4.4hhh;
+	doubled;
 	}
 }	
 
--- tests/thingsToFixInLexjava.java
+++ tests/thingsToFixInLexjava.java
@@ -2,14 +2,14 @@
 
 class Main{
 	static void run(){
-		int a^a;			// exit 2, token je ale invalid	> exit 1
-		int #a;				// Toto projde jako id....
-		int a&a;			//exit 2, token je ale invalid	> exit 1
-		int a\a;			//exit 2, token je ale invalid	> exit 1
-		int 8a;				//dojde 45 intNumber, ale cislo je invalid > exit 1
-		int ř;				//exit 2, token je ale invalid	> exit 1
-		int h4ck0;				//projde
-		int h4ck0řiNámR0zbiliL3x;	//syntax
+	//	int a^a;			// exit 2, token je ale invalid	> exit 1
+	//	int #a;				// Toto projde jako id....
+	//	int a&a;			//exit 2, token je ale invalid	> exit 1
+	//	int a\a;			//exit 2, token je ale invalid	> exit 1
+	//	int 8a;				//dojde 45 intNumber, ale cislo je invalid > exit 1
+	//	int ř;				//exit 2, token je ale invalid	> exit 1
+	//	int h4ck0;				//projde
+	//	int h4ck0řiNámR0zbiliL3x;	//syntax
 	}
 }	
 
