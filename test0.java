7472989e731171919a815c13932e3c356ce06377
Jakub-Svoboda <Svo.jakub@seznam.cz>
2016-12-04 15:29:34 +0100

    commit 4 merge

--- test0.java
+++ test0.java
@@ -1,19 +1,7 @@
 class Main {
 	static void run() {
-		for(int a = 0; a< 10; a=a+1){
-			for(int b = 0;b< 10; b=b+1){
-				if(b== 5 ){
-					continue;
-				}else{
-					ifj16.print(b+" ");
-				}
-				
-				
-			}
-			
-			ifj16.print("    "+ a+ "\n");
-		}
-		
+		int a = 0b0101;
+		ifj16.print("h " +a+ "\n");
 
 	}
 }
