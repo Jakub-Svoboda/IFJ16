5fd4e44e7e93bb1fd414d1e9d20d2233baeba241
Matyas Sladek <sladek.matyas@seznam.cz>
2016-12-04 20:06:46 +0100

    make test working

---
+++ tests/testscriptnew.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo Test output file > tests/testoutput.txt
+
+for i in `ls tests/tests_without_input/*/*`; do
+printf "testing file: $i\n"
+echo ---------------------------------------------$i--------------------------------------------- >> tests/testoutput.txt
+./project $i >> tests/testoutput.txt 2>&1
+#./project $i
+retval=$?
+if [ "$retval" -ne "0" ]
+then 
+echo Navratova hodnota: $retval >> tests/testoutput.txt
+fi
+done
+
+for i in `ls tests/tests_with_input/*.java`; do
+printf "testing file: $i\n"
+a=${i%.*}.txt
+echo ---------------------------------------------$i--------------------------------------------- >> tests/testoutput.txt
+./project $i < $a >> tests/testoutput.txt 2>&1
+#./project $i
+retval=$?
+if [ "$retval" -ne "0" ]
+then 
+echo Navratova hodnota: $retval >> tests/testoutput.txt
+fi
+done
+
+echo
+echo --------------------------------------------
+#comm -2 -3 <(sort testoutput.txt) <(sort testoutputcorrect.txt) > testdiff.txt
+if diff tests/testoutput.txt tests/testoutputcorrect.txt ; then
+echo ALL TESTS OK
+fi
+echo --------------------------------------------
